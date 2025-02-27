d6842a5d49fe7a7b47c0833a4dd6f33cc387bacd
TiagoHub <contato@tiagohub.com>
2025-02-27 00:10:02 -0300

    Create formulario-com-processamento-inicial.php

---
+++ formulario-com-processamento-inicial.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>PHP - Formulário COM Processamento</title>
+<link rel="stylesheet" href="css/style-form-processa.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Formulário e processamento combinados</h1>
+        
+        
+        <form action="" method="post">
+            <div class="mb-3">
+                <label for="nome">Nome:</label>
+                <input type="text" name="nome" id="nome">
+            </div>
+            <div class="mb-3">
+                <label for="email">E-mail:</label>
+                <input type="text" name="email" id="email">
+            </div>
+            <button type="submit" name="enviar">Enviar</button>
+        </form>
+      
+      
+    </div>
+
+
+</body>
+</html>
