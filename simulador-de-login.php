2224b7759d682b4fb205c6d68e87dcc835d71bc6
TiagoHub <contato@tiagohub.com>
2025-03-01 19:01:39 -0300

    Create simulador-de-login.php

---
+++ simulador-de-login.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>PHP - Formulário de Login</title>
+<link rel="stylesheet" href="css/style-form-processa.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Simulador de Login</h1>
+
+        <h2>Login</h2>
+        <form action="" method="post">
+            <div>
+                <label for="nome">E-mail:</label>
+                <input required type="email" name="email" id="email">
+            </div>
+            <div>
+                <label for="senha">Senha:</label>
+                <input required type="password" name="senha" id="senha">
+            </div>
+            <button type="submit" name="entrar">Entrar</button>
+        </form>
+
+        <p>Ou <a href="">cadastre-se</a>!</p>
+
+        <h2>Cadastro</h2>
+        <form action="" method="post">
+            <div>
+                <label for="nome">Nome:</label>
+                <input autofocus required type="text" name="nome" id="nome">
+            </div>
+            <div>
+                <label for="nome">E-mail:</label>
+                <input required type="email" name="email" id="email">
+            </div>
+            <div>
+                <label for="senha">Senha:</label>
+                <input required type="password" name="senha" id="senha">
+            </div>
+            <button type="submit" name="cadastrar">Cadastrar</button>
+        </form>
+ 
+    </div>
+
+
+</body>
+</html>
